e495fd78aa295e9472c56a997f2c841ae312886c
Ramiro Valverde <ramiro.val03@gmail.com>
2023-01-12 19:18:46 -0600

    cambio en el repositorio

---
+++ Assets/Scripts/Control de Versiones/Example.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Course.ControlDeVersiones
+{
+    public class Example : MonoBehaviour
+    {
+        int x;
+
+        private void Start()
+        {
+            x = 5;
+        }
+    }
+}
