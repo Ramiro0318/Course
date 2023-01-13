1b08ad05cb385ecd56d8877290690ba1c9bcbcda
Ramiro Valverde <ramiro.val03@gmail.com>
2023-01-12 19:35:49 -0600

    Added weapon

---
+++ Assets/Scripts/Control de Versiones/Weapon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Course.ControlDeVersiones
+{
+    public class Weapon : MonoBehaviour
+    {    
+        private string title;
+    }
+}
